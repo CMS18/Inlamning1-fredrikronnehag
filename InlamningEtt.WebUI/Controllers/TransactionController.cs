01a88f7d75bc4bc8054a14573b654073fe87fb05
Fredrik Rönnehag <fredrik.ronnehag@gmail.com>
2019-11-11 21:09:50 +0100

    Added controller and view. Tested method for Deposit and Withdraw, POST successfully.

---
+++ InlamningEtt.WebUI/Controllers/TransactionController.cs
@@ -0,0 +1,46 @@
+﻿using InlamningEtt.WebUI.Data;
+using InlamningEtt.WebUI.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace InlamningEtt.WebUI.Controllers
+{
+    public class TransactionController : Controller
+    {
+        private readonly IBankRepository _bankRepository;
+
+        public TransactionController(IBankRepository bankRepository)
+        {
+            _bankRepository = bankRepository;
+        }
+
+        public IActionResult Index()
+        {
+            var model = new TransactionViewModel();
+            return View(model);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Deposit(TransactionViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                // TODO
+            }
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Withdraw(TransactionViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                // TODO
+            }
+
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
---
+++ InlamningEtt.WebUI/Models/TransactionViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace InlamningEtt.WebUI.Models
+{
+    public class TransactionViewModel
+    {
+        public decimal Deposit { get; set; } = 0M;
+        public decimal Withdraw { get; set; } = 0M;
+        public int AccountID { get; set; }
+    }
+}
