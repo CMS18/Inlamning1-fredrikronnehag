5c1510d33ba4fc566bb751756c056f2c8a575cd0
Fredrik Rönnehag <fredrik.ronnehag@gmail.com>
2019-11-11 20:02:16 +0100

    Added skeleton for withdraw/deposit.
    Starting to implement in next commit.

--- InlamningEtt.WebUI/Data/BankRepository.cs
+++ InlamningEtt.WebUI/Data/BankRepository.cs
@@ -7,41 +7,40 @@ namespace InlamningEtt.WebUI.Data
     {
         IEnumerable<Customer> GetCustomers();
         IEnumerable<Account> GetAccounts();
+        void Withdraw(int accountId, decimal amount);
+        void Deposit(int accountId, decimal amount);
+
     }
 
     public class BankRepository : IBankRepository
     {
-        public IEnumerable<Customer> GetCustomers()
+        private static List<Customer> _customers = new List<Customer>
         {
-            return new List<Customer>
-            {
-                new Customer
-                {
-                    Id = 123, AccountId = 20,
-                    Name = "Lars Bengtsson"
-                },
-                new Customer
-                {
-                    Id = 1234, AccountId = 25,
-                    Name = "Erik Eriksson"
-                },
-                new Customer
-                {
-                    Id = 12345, AccountId = 32,
-                    Name = "Anna Svensson"
-                },
-            };
+            new Customer {Id = 123, Name = "Lars Bengtsson"},
+            new Customer {Id = 1234, Name = "Erik Eriksson"},
+            new Customer {Id = 12345, AccountId = 32, Name = "Anna Svensson"},
+        };
+
+        private static List<Account> _accounts = new List<Account>
+        {
+            new Account {Balance = 5000M, CustomerId = 123, Id = 20},
+            new Account {Balance = 5200M, CustomerId = 1234, Id = 25},
+            new Account {Balance = 5820M, CustomerId = 1234, Id = 26},
+            new Account {Balance = 5300M, CustomerId = 12345, Id = 32},
+        };
+
+        public IEnumerable<Customer> GetCustomers() => _customers;
+
+        public IEnumerable<Account> GetAccounts() => _accounts;
+
+        public void Withdraw(int accountId, decimal amount)
+        {
+            throw new System.NotImplementedException();
         }
 
-        public IEnumerable<Account> GetAccounts()
+        public void Deposit(int accountId, decimal amount)
         {
-            return new List<Account>
-            {
-                new Account {Balance = 5000M, CustomerId = 123, Id = 20},
-                new Account {Balance = 5200M, CustomerId = 1234, Id = 25},
-                new Account {Balance = 5820M, CustomerId = 1234, Id = 26},
-                new Account {Balance = 5300M, CustomerId = 12345, Id = 32},
-            };
+            throw new System.NotImplementedException();
         }
     }
 }
